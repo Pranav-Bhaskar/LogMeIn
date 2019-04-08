3c83509241b5f7b4432c85cbb491f2880b2b6c90
Pranav-Bhaskar <30562782+Pranav-Bhaskar@users.noreply.github.com>
2019-04-08 16:09:55 +1100

    Enhanced Windows Support
    This commit has enhanced the windows support of LogMeIn by :
    1. Introducing CLI for windows.
    2. Changing the install location to Program Files.

--- win_install.bat
+++ win_install.bat
@@ -1,20 +1,20 @@
 @echo off
 echo  Ensure that you have python 2.7 installed in the directory "C:\Python27\"
 echo  Copping files
-mkdir C:\logmein
-copy logmein C:\logmein\
-attrib +h C:\logmein
-cd C:\logmein\
+mkdir "%ProgramFiles%\LogMeIn"
+mkdir "%ProgramFiles%\LogMeIn\bin"
+copy /y "%~dp0logmein" "%ProgramFiles%\LogMeIn\"
 echo Enter your details :-
 set /P input=Username : 
-echo %input% > C:\logmein\logmein.data
+echo %input% > "%ProgramFiles%\LogMeIn\logmein.data"
 set /P input=Password : 
-echo %input% >> C:\logmein\logmein.data
+echo %input% >> "%ProgramFiles%\LogMeIn\logmein.data"
 set /P input=Gateway of the host (Example \"192.168.217.111\") :  
-echo %input% >> C:\logmein\logmein.data
+echo %input% >> "%ProgramFiles%\LogMeIn\logmein.data"
 set /P input=Port number of the host (Example \"1000\") (mostly 80/443) :  
-echo %input% >> C:\logmein\logmein.data
-echo @echo off > C:\logmein\logger.bat
-echo C:\Python27\python.exe C:\logmein\logmein >> C:\logmein\logger.bat
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\logger.lnk');$s.TargetPath='C:\logmein\logger.bat';$s.Save()"
+echo %input% >> "%ProgramFiles%\LogMeIn\logmein.data"
+echo @echo off > "%ProgramFiles%\LogMeIn\bin\logmein.bat"
+echo C:\Python27\python.exe "%ProgramFiles%\LogMeIn\logmein" >> "%ProgramFiles%\LogMeIn\bin\logmein.bat"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\logger.lnk');$s.TargetPath='%ProgramFiles%\LogMeIn\bin\logmein.bat';$s.Save()"
+setx path "%path%%ProgramFiles%\LogMeIn\bin\;"
 pause
